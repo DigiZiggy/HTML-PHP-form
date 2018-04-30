623a229a9d4e3cd8a31b1a4021bde1854a07957f
sinare <sinare@ttu.ee>
2018-04-30 14:34:14 +0300

    hw4

--- index.php
+++ index.php
@@ -19,36 +19,18 @@ function validate_todo_item($item) {
     return $errorMessages;
 }
 
-//function print_json($data) {
-//    header("Content-Type: application/json");
-//    print json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-//}
-
-
 if ($cmd === 'main') {
-
     $database_handler = new Database();
     $all_contacts = $database_handler->getAllContacts();
     $data = ['$all_contacts' => $all_contacts];
     $data['$template'] = 'tpl/list.html';
-
-} else if ($cmd === 'ADDlisa') {
+    print render_template('tpl/main.html', $data);
+  //  print render_template('tpl/main.html', ['$all_contacts' => $database_handler->getAllContacts()]);
+} else if ($cmd === 'add'){
     $data['$template'] = 'tpl/secondADDpage.html';
-
-
+    print render_template('tpl/main.html', $data);
 } else if ($cmd === 'save') {
-
-    $url=strtok($_SERVER["REQUEST_URI"],'?');
-    header("Location: ".$url.'?cmd=main');
-
-    if (isset($_POST["firstName"]) & isset($_POST["lastName"]) & isset($_POST["phone1"])) {
-        $errors1 = validate_todo_item($_POST["firstName"]);
-        $errors2 = validate_todo_item($_POST["lastName"]);
-        $errors3 = validate_todo_item($_POST["phone1"]);
-        if (strlen($errors1) == 0 && strlen($errors2) == 0 && strlen($errors3) == 0) {
-
-            try {
-                $connection = new PDO('sqlite:data.sqlite');
+    $connection = new PDO('sqlite:data.sqlite');
                 // set the PDO error mode to exception
                 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 // prepare sql statement to insert contact
@@ -69,38 +51,70 @@ if ($cmd === 'main') {
                         "INSERT INTO numbers (number, contact_id, type) values (?, ?, ?);");
                     $statement->execute(array($_POST["phone3"], $contact_id, 'phone3'));
                 }
-            }
-            catch(PDOException $e)
-            {
-                echo "Connection failed: " . $e->getMessage();
-            }
-        } else {
-            $errors[] = '';
-            array_push($errors, $errors1, $errors2, $errors3);
-            $url=strtok($_SERVER["REQUEST_URI"],'?');
-            header("Location: ".$url.'?cmd=main');
 
-//            $database_handler = new Database();
-//            $all_contacts = $database_handler->getAllContacts();
-//            $data = ['$all_contacts' => $all_contacts];
-//            $data['$errors'] = $errors;
-//            $data['$template'] = 'tpl/list.html';
-
-        }
-     //   $cmd = 'add';
-    }
-} else if ($cmd === 'add') {
-
-  //  print render_template('tpl/main.html', $data);
-
- //   $input = json_decode(file_get_contents("php://input"), true);
-//    $url=strtok($_SERVER["REQUEST_URI"],'?');
-//    header("Location: ".$url.'?cmd=main');
-
-  //  http_response_code(204);
+        //        $url=strtok($_SERVER["REQUEST_URI"],'?');
+                header("Location: ?cmd=main");
 }
 
-
+//if ($cmd === 'main') {
+//
+//    $database_handler = new Database();
+//    $all_contacts = $database_handler->getAllContacts();
+//    $data = ['$all_contacts' => $all_contacts];
+//    $data['$template'] = 'tpl/list.html';
+//
+//} else if ($cmd === 'add') {
+//    $data['$template'] = 'tpl/secondADDpage.html';
+//
+//
+//} else if ($cmd === 'save') {
+//
+//    if (isset($_POST["firstName"]) & isset($_POST["lastName"]) & isset($_POST["phone1"])) {
+//        $errors1 = validate_todo_item($_POST["firstName"]);
+//        $errors2 = validate_todo_item($_POST["lastName"]);
+//        $errors3 = validate_todo_item($_POST["phone1"]);
+//        if (strlen($errors1) == 0 && strlen($errors2) == 0 && strlen($errors3) == 0) {
+//
+//            try {
+//                $connection = new PDO('sqlite:data.sqlite');
+//                // set the PDO error mode to exception
+//                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//                // prepare sql statement to insert contact
+//                $statement = $connection->prepare(
+//                    "INSERT INTO contacts (firstName, lastName) values (?, ?);");
+//                $statement->execute(array($_POST["firstName"],$_POST["lastName"]));
+//                $contact_id = $connection->lastInsertId();
+//                $statement = $connection->prepare(
+//                    "INSERT INTO numbers (number, contact_id, type) values (?, ?, ?);");
+//                $statement->execute(array($_POST["phone1"], $contact_id, 'phone1'));
+//                if (isset($_POST["phone2"]) && !empty($_POST["phone2"])) {
+//                    $statement = $connection->prepare(
+//                        "INSERT INTO numbers (number, contact_id, type) values (?, ?, ?);");
+//                    $statement->execute(array($_POST["phone2"], $contact_id, 'phone2'));
+//                }
+//                if (isset($_POST["phone3"]) && !empty($_POST["phone3"])) {
+//                    $statement = $connection->prepare(
+//                        "INSERT INTO numbers (number, contact_id, type) values (?, ?, ?);");
+//                    $statement->execute(array($_POST["phone3"], $contact_id, 'phone3'));
+//                }
+//
+//                $url=strtok($_SERVER["REQUEST_URI"],'?');
+//                header("Location: ".$url.'?cmd=main');
+//            }
+//            catch(PDOException $e)
+//            {
+//                echo "Connection failed: " . $e->getMessage();
+//            }
+//        } else {
+//            $errors[] = '';
+//            array_push($errors, $errors1, $errors2, $errors3);
+//            $url=strtok($_SERVER["REQUEST_URI"],'?');
+//            header("Location: ".$url.'?cmd=main');
+//        }
+//    }
+//}
+//
+//
 function param($key) {
     if (isset($_GET[$key])) {
         return $_GET[$key];
@@ -110,8 +124,8 @@ function param($key) {
         return '';
     }
 }
-
-print render_template('tpl/main.html', $data);
+//
+//print render_template('tpl/main.html', $data);
 
 
 
