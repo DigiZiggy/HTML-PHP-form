b8731abbb9943a1a3e0eb023640111bf44a2918f
sinare <sinare@ttu.ee>
2018-05-02 12:06:19 +0300

    hw4

--- index.php
+++ index.php
@@ -42,28 +42,28 @@ if ($cmd === 'main') {
     print render_template('tpl/main.html', $data);
 } else if ($cmd === 'save') {
     $connection = new PDO('sqlite:data.sqlite');
-                // set the PDO error mode to exception
-                $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                // prepare sql statement to insert contact
-                $statement = $connection->prepare(
-                    "INSERT INTO contacts (firstName, lastName) values (?, ?);");
-                $statement->execute(array($_POST["firstName"],$_POST["lastName"]));
-                $contact_id = $connection->lastInsertId();
-                $statement = $connection->prepare(
-                    "INSERT INTO numbers (number, contact_id, type) values (?, ?, ?);");
-                $statement->execute(array($_POST["phone1"], $contact_id, 'phone1'));
-                if (isset($_POST["phone2"]) && !empty($_POST["phone2"])) {
-                    $statement = $connection->prepare(
-                        "INSERT INTO numbers (number, contact_id, type) values (?, ?, ?);");
-                    $statement->execute(array($_POST["phone2"], $contact_id, 'phone2'));
-                }
-                if (isset($_POST["phone3"]) && !empty($_POST["phone3"])) {
-                    $statement = $connection->prepare(
-                        "INSERT INTO numbers (number, contact_id, type) values (?, ?, ?);");
-                    $statement->execute(array($_POST["phone3"], $contact_id, 'phone3'));
-                }
+    // set the PDO error mode to exception
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // prepare sql statement to insert contact
+    $statement = $connection->prepare(
+        "INSERT INTO contacts (firstName, lastName) values (?, ?);");
+    $statement->execute(array($_POST["firstName"],$_POST["lastName"]));
+    $contact_id = $connection->lastInsertId();
+    $statement = $connection->prepare(
+        "INSERT INTO numbers (number, contact_id, type) values (?, ?, ?);");
+    $statement->execute(array($_POST["phone1"], $contact_id, 'phone1'));
+    if (isset($_POST["phone2"]) && !empty($_POST["phone2"])) {
+        $statement = $connection->prepare(
+            "INSERT INTO numbers (number, contact_id, type) values (?, ?, ?);");
+        $statement->execute(array($_POST["phone2"], $contact_id, 'phone2'));
+    }
+    if (isset($_POST["phone3"]) && !empty($_POST["phone3"])) {
+        $statement = $connection->prepare(
+            "INSERT INTO numbers (number, contact_id, type) values (?, ?, ?);");
+        $statement->execute(array($_POST["phone3"], $contact_id, 'phone3'));
+    }
 
-                header("Location: ?cmd=main");
+    header("Location: ?cmd=main");
 }
 
 //if ($cmd === 'main') {
@@ -125,6 +125,8 @@ if ($cmd === 'main') {
 //}
 //
 //
+
+
 function param($key) {
     if (isset($_GET[$key])) {
         return $_GET[$key];
